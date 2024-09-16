d8b6eb08d68faea17c7c33c0fbe8388a6f48f96b
Nugar <cladeadenugar@gmail.com>
2024-09-16 10:04:37 +0100

    new

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <img
+          src="https://res.cloudinary.com/thirtythree/image/upload/v1726476802/vicmob-logo_vc17p3.jpg"
+          alt="vicmob-logo"
+          className="w-[50px] h-[50px] rounded-full"
+        />
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
