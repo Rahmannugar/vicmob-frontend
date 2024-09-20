69aa17c771ed16cac925c76757312bcd69acc957
Nugar <cladeadenugar@gmail.com>
2024-09-20 12:37:19 +0100

    completed loading interface

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,45 @@ import Contact from "./pages/Contact";
 import About from "./pages/About";
 import Curriculum from "./pages/Curriculum";
 import Admissions from "./pages/Admissions";
-// vicmobschools@gmail.com
+import { useEffect, useState } from "react";
 
 const App = () => {
+  const [hasMounted, setHasMounted] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setHasMounted(true);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="overflow-hidden">
-      <Navbar />
-      <Routes>
-        <Route element={<Home />} path="/" />
-        <Route element={<Contact />} path="/contact" />
-        <Route element={<About />} path="/about" />
-        <Route element={<Curriculum />} path="/curriculum" />
-        <Route element={<Admissions />} path="/admissions" />
-      </Routes>
-      <Footer />
+      {!hasMounted ? (
+        <div className="flex flex-col space-y-3 justify-center h-screen items-center">
+          <img
+            src="https://res.cloudinary.com/thirtythree/image/upload/v1726476802/vicmob-logo_vc17p3.jpg"
+            alt="vicmob-logo"
+            className="w-[100px] h-[100px] rounded-full spinner"
+          />
+          <h1 className="font-bold text-blue-900 text-xl">VICMOB SCHOOLS</h1>
+        </div>
+      ) : (
+        <div>
+          <Navbar />
+          <Routes>
+            <Route element={<Home />} path="/" />
+            <Route element={<Contact />} path="/contact" />
+            <Route element={<About />} path="/about" />
+            <Route element={<Curriculum />} path="/curriculum" />
+            <Route element={<Admissions />} path="/admissions" />
+          </Routes>
+          <Footer />
+        </div>
+      )}
     </div>
   );
 };
+
 export default App;
