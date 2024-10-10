6f7d8108e052c598d31c6a6d4af3a8378b06f684
Nugar <cladeadenugar@gmail.com>
2024-10-10 11:30:16 +0100

    new

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,6 +1,7 @@
-import { useState, useEffect } from "react";
-import { motion } from "framer-motion";
 import Typewriter from "typewriter-effect";
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { div } from "framer-motion/client";
 
 const Carousel = () => {
   const images = [
@@ -9,45 +10,22 @@ const Carousel = () => {
     "https://res.cloudinary.com/thirtythree/image/upload/v1726529414/vicmob-3_bjoejg.jpg",
   ];
 
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 5000);
-    return () => clearInterval(interval);
-  }, [images.length]);
-
   return (
     <div className="relative overflow-hidden">
       <div className="relative">
-        <motion.div
-          className="flex md:hidden"
-          initial={{ x: 0 }}
-          animate={{ x: `-${currentIndex * 100}%` }}
-          transition={{ type: "tween", ease: "easeInOut", duration: 4 }}
-        >
+        <div className="flex items-center cursor-grab active:cursor-grabbing">
           {images.map((img, index) => (
-            <motion.img
-              whileHover={{ scale: 2 }}
-              drag
+            <div
               key={index}
-              className="h-[500px] object-cover w-screen"
-              src={img}
-              alt={`Carousel-${index + 1}`}
-            />
+              style={{
+                backgroundImage: `url(${img})`,
+                backgroundSize: `cover`,
+                backgroundPosition: "center",
+              }}
+              className="aspect-video h-screen w-screen"
+            ></div>
           ))}
-        </motion.div>
-
-        <motion.img
-          className="h-[600px] hidden md:block object-cover w-screen"
-          src={images[currentIndex]}
-          alt={`Carousel-${currentIndex + 1}`}
-          initial={{ opacity: 0 }}
-          drag
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.5 }}
-        />
+        </div>
 
         <div className="absolute z-10 top-28 left-[3%]">
           <p className="bg-blue-950 text-sm font-bold text-white w-[30vw] min-w-[200px] p-3">
