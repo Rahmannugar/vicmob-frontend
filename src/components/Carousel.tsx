088a9be84df8eff017a84046752c6ae17c5ac493
Nugar <cladeadenugar@gmail.com>
2024-10-10 11:04:33 +0100

    new

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,6 +1,5 @@
-import { useState } from "react";
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 import Typewriter from "typewriter-effect";
 
 const Carousel = () => {
@@ -12,59 +11,45 @@ const Carousel = () => {
 
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  const handlePrev = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? images.length - 1 : prevIndex - 1
-    );
-  };
-
-  const handleNext = () => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-  };
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 5000);
+    return () => clearInterval(interval);
+  }, [images.length]);
 
   return (
     <div className="relative overflow-hidden">
       <div className="relative">
-        <div
-          className="flex md:hidden transition-transform duration-500 ease-in-out"
-          style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+        <motion.div
+          className="flex md:hidden"
+          initial={{ x: 0 }}
+          animate={{ x: `-${currentIndex * 100}%` }}
+          transition={{ type: "tween", ease: "easeInOut", duration: 4 }}
         >
           {images.map((img, index) => (
-            <img
+            <motion.img
+              whileHover={{ scale: 2 }}
+              drag
               key={index}
               className="h-[500px] object-cover w-screen"
               src={img}
               alt={`Carousel-${index + 1}`}
             />
           ))}
-        </div>
+        </motion.div>
 
-        <img
+        <motion.img
           className="h-[600px] hidden md:block object-cover w-screen"
-          src="https://res.cloudinary.com/thirtythree/image/upload/v1726476546/vicmob-1_cwnkls.jpg"
-          alt="Carousel-1"
+          src={images[currentIndex]}
+          alt={`Carousel-${currentIndex + 1}`}
+          initial={{ opacity: 0 }}
+          drag
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5 }}
         />
 
-        {/* sliders */}
-        <div className="md:hidden">
-          <div
-            className="bg-white p-2 rounded-full absolute bottom-40 z-20 left-2 cursor-pointer"
-            onClick={handlePrev}
-          >
-            <ChevronLeftIcon />
-          </div>
-          <div
-            className="bg-white p-2 rounded-full absolute bottom-40 z-20 right-2 cursor-pointer"
-            onClick={handleNext}
-          >
-            <ChevronRightIcon />
-          </div>
-        </div>
-
         <div className="absolute z-10 top-28 left-[3%]">
-          <h1 className=" font-black text-lg bg-white p-3 w-[30vw] min-w-[200px]">
-            Vicmob Schools
-          </h1>
           <p className="bg-blue-950 text-sm font-bold text-white w-[30vw] min-w-[200px] p-3">
             <Typewriter
               options={{
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,33 +1,47 @@
+import { motion } from "framer-motion";
 const Navbar = () => {
   return (
     <nav className="relative z-50 border-b">
       <div className="bg-blue-950 w-screen p-3 md:p-7 flex md:justify-between justify-center items-center">
-        <a href="/" className="flex items-center space-x-4">
+        <motion.a href="/" className="flex items-center space-x-4">
           <img
             src="https://res.cloudinary.com/thirtythree/image/upload/v1726476802/vicmob-logo_vc17p3.jpg"
             alt="vicmob-logo"
             className="w-[50px] h-[50px] lg:w-[70px] lg:h-[70px] rounded-full"
           />
-          <h1 className="text-white text-sm font-bold">VICMOB SCHOOLS</h1>
-        </a>
+          <div className="flex flex-col space-y-1">
+            <h1 className="text-white text-sm font-bold">
+              VICMOB <span className="text-yellow-300">SCHOOLS</span>
+            </h1>
+            <span className="text-white text-[10px] font-bold">
+              Home Of Champions
+            </span>
+          </div>
+        </motion.a>
 
         {/* desktop navbar */}
         <div className="text-white font-bold hidden md:flex space-x-4">
-          <a href="/" className="hover:opacity-40 duration-100">
+          <motion.a href="/" className="hover:opacity-40 duration-100">
             Home
-          </a>
-          <a href="/admissions" className="hover:opacity-40 duration-100">
+          </motion.a>
+          <motion.a
+            href="/admissions"
+            className="hover:opacity-40 duration-100"
+          >
             Admission
-          </a>
-          <a href="/curriculum" className="hover:opacity-40 duration-100">
+          </motion.a>
+          <motion.a
+            href="/curriculum"
+            className="hover:opacity-40 duration-100"
+          >
             Curriculum
-          </a>
-          <a href="/contact" className="hover:opacity-40 duration-100">
+          </motion.a>
+          <motion.a href="/contact" className="hover:opacity-40 duration-100">
             Contact form
-          </a>
-          <a href="/about" className="hover:opacity-40 duration-100">
+          </motion.a>
+          <motion.a href="/about" className="hover:opacity-40 duration-100">
             About us
-          </a>
+          </motion.a>
         </div>
       </div>
     </nav>
