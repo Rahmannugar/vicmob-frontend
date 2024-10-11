07b5791877785f981e81709b10d2eb6b4fa6f60e
Nugar <cladeadenugar@gmail.com>
2024-10-11 15:19:48 +0100

    new(new)

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { motion, useMotionValue } from "framer-motion";
+import { motion } from "framer-motion";
 import Typewriter from "typewriter-effect";
 
 const Carousel = () => {
