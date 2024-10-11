9d4ebc9dd82de5f7e4fd7867ed834d9b4920a626
Nugar <cladeadenugar@gmail.com>
2024-10-11 13:26:01 +0100

    new

--- src/components/Schools.tsx
+++ src/components/Schools.tsx
@@ -4,50 +4,88 @@ import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 const Schools = () => {
   return (
     <div>
-      <h1 className="text-center text-lg bg-blue-900 font-black mt-10 text-white py-5">
+      <h1 className="text-center text-lg font-black mt-10 bg-black text-white py-5">
         <SchoolIcon fontSize="medium" /> Vicmob Schools
       </h1>
 
-      <div className="md:flex items-center px-10 md:px-0 lg:space-x-20 md:space-x-10 justify-center mt-6 md:mt-10">
-        {/* primary school */}
-        <div
-          className="flex flex-col relative border-4 border-blue-900
-"
-        >
+      {/* Updated grid layout */}
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-10 px-10 md:px-0 mt-7 md:mt-10">
+        {/* Early school */}
+        <div className="flex flex-col items-center justify-center text-center relative">
           <img
-            className="object-cover md:w-[300px] lg:w-[450px] lg:h-[400px] h-[300px] "
-            src="https://res.cloudinary.com/thirtythree/image/upload/v1726713037/primary_s67lxw.jpg"
-            alt="primary"
+            className="object-cover object-center w-full lg:w-[450px] lg:h-[400px] h-[300px]"
+            src="https://res.cloudinary.com/thirtythree/image/upload/v1728647468/early-years_mdknqs.jpg"
+            alt="Early years"
           />
-          <button className="text-sm bg-blue-900 text-white py-2 font-bold">
-            Vicmob Nursery and Primary school
+          <button className="text-sm bg-black text-white py-3 font-bold w-full lg:w-[450px]">
+            Our Early Years
           </button>
-          <p className="text-justify py-4 text-xs md:max-w-[300px] px-3 lg:max-w-[450px] md:h-[140px] lg:h-[120px]">
-            Welcome to Vicmob Nursery & Primary School, where we provide an
+          <p className="text-justify py-4 text-xs px-3 md:max-w-[300px] lg:max-w-[450px] md:h-[140px] lg:h-[120px]">
+            Welcome to our early years programme, where we provide an
             exceptional educational foundation for children, nurturing their
             growth and development in a caring and engaging environment.
           </p>
-          <button className="bg-blue-900 py-3 px-4 text-white hover:bg-white hover:border-black hover:border-t hover:text-blue-900 duration-00">
+          <button className="bg-black py-3 w-full lg:w-[450px] text-white text-sm">
             Visit school <ArrowForwardIcon />
           </button>
         </div>
 
-        {/* secondary school */}
-        <div className="mt-10 md:mt-0 flex flex-col relative border-4 border-blue-900">
+        {/* Junior school */}
+        <div className="flex flex-col items-center justify-center text-center relative">
           <img
-            className="object-cover  lg:w-[450px] lg:h-[400px] md:w-[300px] h-[300px]"
-            src="https://res.cloudinary.com/thirtythree/image/upload/v1726712728/secondary_zhcrqb.jpg"
-            alt="secondary"
+            className="object-cover w-full lg:w-[450px] lg:h-[400px] h-[300px]"
+            src="https://res.cloudinary.com/thirtythree/image/upload/v1728618597/photo_5785276873213854741_y_emiw1h.jpg"
+            alt="Junior"
           />
-          <button className="text-sm bg-blue-900 text-white py-3 font-bold">
-            Vicmob High school
+          <button className="text-sm bg-red-700 text-white py-3 font-bold w-full lg:w-[450px]">
+            Our Junior School
           </button>
           <p className="text-justify py-4 text-xs px-3 md:max-w-[300px] lg:max-w-[450px] md:h-[140px] lg:h-[120px]">
-            At Vicmob High School, we shape future leaders by offering a
-            comprehensive education that prioritizes excellence and fosters a
-            strong sense of social accountability.
+            Our Junior School offers a nurturing environment where we help young
+            learners explore their interests and develop strong academic and
+            interpersonal skills.
           </p>
-          <button className="bg-blue-900 py-3 px-4 text-white hover:bg-white hover:border-black hover:border-t text-sm hover:text-blue-900 duration-00">
+          <button className="bg-red-700 py-3 w-full lg:w-[450px] text-white text-sm">
+            Visit school <ArrowForwardIcon />
+          </button>
+        </div>
+
+        {/* Middle school */}
+        <div className="flex flex-col items-center justify-center text-center relative">
+          <img
+            className="object-cover w-full lg:w-[450px] lg:h-[400px] h-[300px]"
+            src="https://res.cloudinary.com/thirtythree/image/upload/v1728618624/IMG-20240706-WA0064_ha3c0s.jpg"
+            alt="Middle"
+          />
+          <button className="text-sm bg-purple-900 text-white py-3 font-bold  w-full lg:w-[450px]">
+            Our Middle School
+          </button>
+          <p className="text-justify py-4 text-xs px-3 md:max-w-[300px] lg:max-w-[450px] md:h-[140px] lg:h-[120px]">
+            In our Middle School, we guide students through their early teenage
+            years by focusing on both academic excellence and personal growth,
+            preparing them for future challenges.
+          </p>
+          <button className="bg-purple-900 py-3  w-full lg:w-[450px] text-white text-sm">
+            Visit school <ArrowForwardIcon />
+          </button>
+        </div>
+
+        {/* High school */}
+        <div className="flex flex-col items-center justify-center text-center relative">
+          <img
+            className="object-cover w-full lg:w-[450px] lg:h-[400px] h-[300px]"
+            src="https://res.cloudinary.com/thirtythree/image/upload/v1728618598/IMG-20240628-WA0005_hxgh76.jpg"
+            alt="High"
+          />
+          <button className="text-sm bg-green-900 text-white py-3 font-bold w-full lg:w-[450px]">
+            Our High School
+          </button>
+          <p className="text-justify py-4 text-xs px-3 md:max-w-[300px] lg:max-w-[450px] md:h-[140px] lg:h-[120px]">
+            At Vicmob High, we shape future leaders by offering a comprehensive
+            education that prioritizes excellence and fosters a strong sense of
+            social accountability.
+          </p>
+          <button className="bg-green-900 py-3 w-full lg:w-[450px] text-white text-sm">
             Visit school <ArrowForwardIcon />
           </button>
         </div>
@@ -55,4 +93,5 @@ const Schools = () => {
     </div>
   );
 };
+
 export default Schools;
